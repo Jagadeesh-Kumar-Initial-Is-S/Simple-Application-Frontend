868d09666488ae8264a663a702790f9e7dc61868
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-10 19:04:35 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Advertisement from './Advertisement';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Advertisement />
     <App />
+    <Advertisement />
   </React.StrictMode>
 );
 
